0f4a167e53b9c1eda6a5cae3201e2b60017d8e01
cheboi <moscheb96@gmail.com>
2025-02-24 15:21:00 +0300

    fix: unused subheader

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,37 +3,36 @@
 import Link from "next/link";
 import { ShoppingCart } from "lucide-react";
 import { useCart } from "@/app/context/CartContext";
-import SubHeader from "./SubHeader";
 
 export default function Header() {
   const { cart } = useCart();
 
   return (
     <>
-    <header className="bg-primary-dark text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">M-Safi</h1>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <Link href="/" className="hover:text-secondary-light">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href="/cart" className="hover:text-secondary-light">
-              <ShoppingCart size={20} />
-              {cart.length > 0 && (
+      <header className="bg-primary-dark text-white p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-2xl font-bold">M-Safi</h1>
+          <nav>
+            <ul className="flex space-x-4">
+              <li>
+                <Link href="/" className="hover:text-secondary-light">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href="/cart" className="hover:text-secondary-light">
+                  <ShoppingCart size={20} />
+                  {cart.length > 0 && (
                     <span className="absolute top-1 right-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
                       {cart.length}
                     </span>
                   )}
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     </>
   );
 }
